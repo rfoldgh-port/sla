aa4b5829b453fa1c2a046b860b57d11e365a9f36
Jon Holmberg <Jon Holmberg>
2017-02-24 00:36:25 -0800

    connectingg yelp api

--- server.js
+++ server.js
@@ -6,10 +6,30 @@ var mongoose = require('mongoose');
 var config = require('./config');
 var http = require('http');
 var User = require('./models/userModel');
+var session = require('express-session');
 
+var MongoStore = require('connect-mongo')(session);
 app.use(express.static('static'));
 
+app.use(session({
+  secret: 'meda',
+  resave: true,
+  saveUninitialized: true,
+  store: new MongoStore({ mongooseConnection: mongoose.connection })
+}));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
 
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
 
@@ -31,13 +51,7 @@ var runServer = function(callback) {
 };
 
 
-if (require.main === module) {
-    runServer(function(err) {
-        if (err) {
-            console.error(err);
-        }
-    });
-};
+
 
 passport.use(new GoogleStrategy({
     clientID: '718688875995-nn8i6hhq6n8391qoikoti959v7l1ctad.apps.googleusercontent.com',
@@ -77,19 +91,31 @@ app.get('/auth/google/callback',
 
 app.use(express.static('/build'));
  
- // //Yelp API request
- // var api = http.createClient(80, 'https://api.yelp.com');
+ //Yelp API request
+ // var api = http.request(80, 'https://api.yelp.com');
  
  // var request = api.request('GET', '/v2/search?term=food&location=San+Francisco',
  // {
-		// 'host': 'https://api.yelp.com',
+	
 		// 'accept': 'application/json',
 		// 'api-key': 'cqfxiVfo3jj8F8016f2uxQ'
  // });
  
- // request.on('response', function(response){});
- // request.end();
-
+ var request = require('request');
+request('https://api.yelp.com/cqfxiVfo3jj8F8016f2uxQ/v2/search?term=food&location=San+Francisco&limit=1', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
+ 
+ 
+if (require.main === module) {
+    runServer(function(err) {
+        if (err) {
+            console.error(err);
+        }
+    });
+};
  
 module.exports.runServer = runServer;
 module.exports.app = app;
