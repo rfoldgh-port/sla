b7b6f820930431e8c550e453d1fca12ee8a2a3ab
Jon Holmberg <Jon Holmberg>
2017-03-15 00:17:06 -0700

    add nav and footer

--- js/components/homepage.js
+++ js/components/homepage.js
@@ -1,32 +1,62 @@
 import React from 'react';
 import SearchFormContainer from "./ski-search-form";
 
-export default class Homepage extends React.Component { 
+export default class Homepage extends React.Component {
     constructor(props) {
-        super(props);   
+        super(props);
 
-	
-    }   
-	
-  
+
+    }
+
+
     render() {
-	
+
 	return (
-				
+
 					<div className="SkiListFavorite">
+                  <nav>
+                    <div class="nav-wrapper">
+                      <a href="#" class="brand-logo right">Logo</a>
+                      <ul id="nav-mobile" class="left hide-on-med-and-down">
+                        <li><a href="#">Sass</a></li>
+                        <li><a href="#">Components</a></li>
+                        <li><a href="#">JavaScript</a></li>
+                      </ul>
+                    </div>
+                  </nav>
+
 						<h1>Welcome to the Ski Lift App!</h1>
 						<p>Here you can search for ski resorts by region. Feel free to save them to your account for use later!</p>
 						<SearchFormContainer />
+
+
+
+            <footer class="page-footer">
+                 <div class="container">
+                   <div class="row">
+                     <div class="col l6 s12">
+                       <h5 class="white-text">Footer Content</h5>
+                     </div>
+                     <div class="col l4 offset-l2 s12">
+                     </div>
+                   </div>
+                 </div>
+                 <div class="footer-copyright">
+                   <div class="container">
+                  <p> © 2014 Copyright Text</p>
+                   <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+                   </div>
+                 </div>
+            </footer>
+
 					</div>
-				
-            
+
+
 
 		);
 	}
 };//END CLASS
-        
+
 // const mapStateToProps = (state, props) => ({
 
 // });
-
-
--- js/components/ski-search-form.js
+++ js/components/ski-search-form.js
@@ -7,28 +7,28 @@ import {getSkiInfo} from '../actions/index';
 import {addSkiFavorite} from '../actions/index';
 import {showSkiResorts} from '../actions/index';
 
-export class SearchForm extends React.Component { 
+export class SearchForm extends React.Component {
     constructor(props) {
-        super(props);      
+        super(props);
 
 		this.searchSkiLifts = this.searchSkiLifts.bind(this);
 		// this.addFavorite = this.addFavorite.bind(this);
 		this.showSkiFavorite = this.showSkiFavorite.bind(this);
-		
+
     }
 
     searchSkiLifts(event){
-       event.preventDefault();
-	this.props.dispatch(getSkiInfo(this.inputText.value));
-	 window.location="/#/search";
+        event.preventDefault();
+	      this.props.dispatch(getSkiInfo(this.inputText.value));
+	      window.location="/#/search";
 	}
-	
+
 	// addFavorite(event){
 		// event.preventDefault();
 		// this.props.dispatch(addSkiFavorite(this.inputText.value));
-		
+
 	// }
-    
+
 	showSkiFavorite(event){
 		event.preventDefault();
 		this.props.dispatch(showSkiResorts());
@@ -37,31 +37,31 @@ export class SearchForm extends React.Component {
 
 
      render() {
-		 
+
 		 var buttonColor = {
 				backgroundColor: "#ff1100"
 		 };
-        
+
         let form;
               form = ( <div><form className="ski-lift-search" onSubmit={this.searchSkiLifts}>
                 <input className ="ski-lift-input" type="text" ref={(input) => this.inputText = input } name="term-input"/>
                 <div className="seach-favorite-buttons"><button style={buttonColor} className="btn waves-effect waves-light" type='submit' id="search-button">Search</button>
 				<button style={buttonColor} className="btn waves-effect waves-light" type='submit' id="show-favorites" onClick={this.showSkiFavorite}>Show Favorites</button></div>
-				
-                
+
+
                 &nbsp;
             </form></div>); //JSX syntax where you can use html markup as JS objects
-			
-            
+
+
             return (
-			
+
                 <div className="searchForm">
 					<div className="row">
 						<a className="google-button" href='/auth/google'><img className="google-image" src='../images/sign-in-with-google.png'/></a>
 						<div>{form}</div>
 					</div>
                 </div>
-    
+
             );
          }
 }
