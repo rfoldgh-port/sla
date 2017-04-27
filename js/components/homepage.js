643c452b4f76e8a070021fb89cb501c841ab257b
Jon Holmberg <Jon Holmberg>
2017-04-26 20:28:35 -0700

    finish landing page

--- js/components/homepage.js
+++ js/components/homepage.js
@@ -48,24 +48,24 @@ export class Homepage extends React.Component {
                  </div>
               </nav>
 
-						<div className="homepage-text"><h1>Welcome to the Ski Lift App!</h1>
-						<p>Here you can search for ski resorts by region. Feel free to save them to your account for use later!</p>
+						<div className="homepage-text"><h1>SkiSearch</h1>
+						<p id="intro-message-p">Here you can search for ski resorts by region. Feel free to save them to your account for use later!</p>
             <a className ="waves-effect waves-light btn" href="/#/search-form">Click here to get started searching for your favorite ski resorts.</a><br></br>
             {login}
             </div>
-            <div className="features-sections">
+          <div className="features-sections">
               <div className="image-container">
                   <div className="feature-block">
-                    <img className="feature-img" src="../../images/snow-mountains-winter-sport.jpg" />
-                    <p className="feature-description">Feature description goes here</p>
+                    <img id="ski-lift-img" className="feature-img" src="../../images/snow-mountains-winter-sport.jpg" />
+                    <p id="location-description" className="feature-description">Look up ski resort destinations by location.</p>
                   </div>
                   <div className="feature-block">
-                    <img className="feature-img" src="../../images/skier-on-slope.jpeg" />
-                    <p className="feature-description">Feature description goes here</p>
+                    <p id="ratings-description" className="feature-description">View ski resort info including ratings and links for booking.</p>
+                    <img id="skier-img" className="feature-img" src="../../images/skier-on-slope.jpeg" />
                   </div>
                   <div className="feature-block">
-                    <img className="feature-img" src="../../images/tree-slopes.jpeg" />
-                    <p className="feature-description">Feature description goes here</p>
+                    <img id="mountain-img" className="feature-img" src="../../images/tree-slopes.jpeg" />
+                    <p id="google-description" className="feature-description">Log in with your Google account to save ski resort information for viewing later.</p>
                   </div>
                 </div>
             </div>
