66e3f11c3c8cbc4e3fb7324385026de6154a572f
Jon Holmberg <Jon Holmberg>
2017-05-02 19:41:29 -0700

    fix server.js config.js

--- config.js
+++ config.js
@@ -2,7 +2,10 @@
 
 exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/thinkful-jon';
 //in a real world setting you’d have that set to a local variable, but assuming this is just a learning project, that should work
+exports.GOOGLE_CALLBACK = process.env.GOOGLE_CALLBACK;
 
-
+exports.GOOGLE_ID = process.env.GOOGLE_ID;
+exports.GOOGLE_SECRET = process.env.GOOGLE_SECRET;
+exports.GOOGLE_CALLBACK = process.env.GOOGLE_CALLBACK;
 
 exports.PORT = process.env.PORT || 8080;
