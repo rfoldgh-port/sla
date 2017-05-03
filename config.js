66314a8bc1ac99b80e0aad0853080d593e3e7b6b
Jon Holmberg <Jon Holmberg>
2017-05-02 19:04:56 -0700

    fix config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
 //in a real world setting you’d have that set to a local variable, but assuming this is just a learning project, that should work
                       global.DATABASE_URL ||
                       (process.env.NODE_ENV === 'production' ?
-                              process.env.MONGO_URI : 'mongodb://<Holmberg18>:<Patience1!>@ds147975.mlab.com:47975/thinkful-jon');
+                              process.env.MONGO_URI : 'mongodb://Holmberg18:Patience1!@ds147975.mlab.com:47975/thinkful-jon');
 exports.PORT = process.env.PORT || 8080;
